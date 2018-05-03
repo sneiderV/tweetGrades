2b7f4c1b4bd7a2f2309e3ed42af1549b4ff4ecdc
sneiderV <e.velandia2164@uniandes.edu.co>
2018-05-02 23:56:20 -0500

    add conection between imports and client

--- client/main.js
+++ client/main.js
@@ -1,9 +1,20 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+//import { Template } from 'meteor/templating';
+//import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import App from "../imports/ui/App.js"
 
-Template.hello.onCreated(function helloOnCreated() {
+
+Meteor.startup(()=>{
+	{
+		render(<App />,document.getElementById("react-root"));	
+	}
+});
+
+/*Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
@@ -19,4 +30,4 @@ Template.hello.events({
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
-});
+});*/
---
+++ imports/ui/App.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+
+export default class App extends Component{
+
+	render(){
+		return(
+			
+		//div inicial
+		<div>
+
+			<h1>corriendo desde app.js en imports</h1>	
+		</div>
+		);
+	}
+
+}
