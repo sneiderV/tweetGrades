6ae681e55afda5e5b0869b9741c983502d298652
Sneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2018-05-15 00:29:31 -0500

    created a method for GET tweets

--- imports/api/students.js
+++ imports/api/students.js
@@ -10,4 +10,28 @@ if (Meteor.isServer) {
   Meteor.publish('students', function studentsPublication() {
     return Students.find();
   });
-}
+
+  Meteor.methods({
+
+  	'darTweets'(){
+  		var Twit = require('twit');
+
+	    var T = new Twit({
+	        consumer_key:         'GTi2PxIHdvA8OjneUlZKf7XXx', // API key
+	        consumer_secret:      'qC0fVvdNiQ8P18Qxe3G176gZ5M7ERykq0FwSVPvpMBB5wTtDUb', // API secret
+	        access_token:         '889371522648993792-vnmSDq4hyVQDY4aMUGkEzrO4fKOXOLB', 
+	        access_token_secret:  'ILhpnYZ85aJjNeJ3a8N2DFo8Beo42QqYNnmUHqCgMfSS9'
+	    });
+
+	    //esto es algo raro ????????
+	    const getTweets = Meteor.wrapAsync(T.get, T);
+	    //  search twitter for all tweets containing the word '#WebDev @Uniandes' /// since:2011-11-11
+	    try {
+	      return getTweets('search/tweets', { q: '#WebDev @Uniandes', count: 100 });
+    	} 
+    	catch (error) { throw new Meteor.Error(error.name, error.message);}
+  	}
+
+  }); // final of Meteor Methods
+
+}//final of if Meteor.isServer
